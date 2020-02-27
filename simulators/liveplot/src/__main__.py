f2873f8cb8e2377dd0ae5aaa54cc0c552e172bde
david <dleroys@umich.edu>
2020-02-26 22:40:49 -0500

    [Localization] it works....kinda

--- simulators/liveplot/src/__main__.py
+++ simulators/liveplot/src/__main__.py
@@ -7,7 +7,7 @@ import sys
 import numpy as np
 
 
-SAMPLE_LENGTH = 5
+SAMPLE_LENGTH = 10
 style.use('fivethirtyeight')
 
 
@@ -18,20 +18,50 @@ class LivePlotter():
     '''
     def __init__(self):
         self.length = SAMPLE_LENGTH
-        self.interval = 5
+        self.interval = 1
         self.log_path = sys.argv[1]
         self.gps_data = self.readCSV('gps', float, True)
         self.odom_data = self.readCSV('odom', float, True)
         self.movAvg_data = self.readCSV('movAvg', float, True)
+        self.gps_data = {'long_deg': self.gps_data['long_deg'],
+                         'long_min': self.gps_data['long_min'],
+                         'lat_deg': self.gps_data['lat_deg'],
+                         'lat_min': self.gps_data['lat_min']}
+        self.odom_data = {'long_deg': self.odom_data['long_deg'],
+                          'long_min': self.odom_data['long_min'],
+                          'lat_deg': self.odom_data['lat_deg'],
+                          'lat_min': self.odom_data['lat_min']}
+        self.movAvg_data = {'long_deg': self.movAvg_data['long_deg'],
+                            'long_min': self.movAvg_data['long_min'],
+                            'lat_deg': self.movAvg_data['lat_deg'],
+                            'lat_min': self.movAvg_data['lat_min']}
+        self.gps_data['lon'] = [degs + mins for degs, mins in
+                                zip(self.gps_data['long_deg'], self.gps_data['long_min'])]
+        self.gps_data['lat'] = [degs + mins for degs, mins in
+                                zip(self.gps_data['lat_deg'], self.gps_data['lat_min'])]
+        self.odom_data['lon'] = [degs + mins for degs, mins in
+                                 zip(self.odom_data['long_deg'], self.gps_data['long_min'])]
+        self.odom_data['lat'] = [degs + mins for degs, mins in
+                                 zip(self.odom_data['lat_deg'], self.gps_data['lat_min'])]
+        self.movAvg_data['lon'] = [degs + mins for degs, mins in
+                                   zip(self.movAvg_data['long_deg'], self.gps_data['long_min'])]
+        self.movAvg_data['lat'] = [degs + mins for degs, mins in
+                                   zip(self.movAvg_data['lat_deg'], self.gps_data['lat_min'])]
+        print(self.gps_data)
         self.gps_window_start = 0
         self.gps_window_end = self.length
-        self.odom__window_start = 0
+        self.odom_window_start = 0
         self.odom_window_end = self.length
         self.movAvg_window_start = 0
         self.movAvg_window_end = self.length
-        self.gps_window = self.gps_data[:self.length]
-        self.odom_window = self.odom_data[:self.length]
-        self.movAvg_window = self.movAvg_data[:self.length]
+        self.gps_window = [self.gps_data['lon'][self.gps_window_start:self.gps_window_end],
+                           self.gps_data['lat'][self.gps_window_start:self.gps_window_end]]
+        self.odom_window = [self.odom_data['lon'][self.odom_window_start:self.odom_window_end],
+                            self.odom_data['lat'][self.odom_window_start:self.odom_window_end]]
+        self.movAvg_window = [self.movAvg_data['lon']
+                              [self.movAvg_window_start:self.movAvg_window_end],
+                              self.movAvg_data['lat']
+                              [self.movAvg_window_start:self.movAvg_window_end]]
 
     def readCSV(self, type, dtype, names_in):
         # Reads in the CSV file specified by log
@@ -43,37 +73,45 @@ class LivePlotter():
     def run(self):
         self.fig = plt.figure()
         self.ax1 = self.fig.add_subplot(1, 1, 1)
-        self.ax1.set_xlim([-.0002, .0002])
-        self.ax1.set_ylim([-.0002, .0002])
-        self.lines = [] * 3
+        self.ax1.set_xlim([-1, 1])
+        self.ax1.set_ylim([-1, 1])
+        self.lines = [self.ax1.plot([self.gps_window[0]],
+                      [self.gps_window[1]], '-o', label='Raw')[0],
+                      self.ax1.plot([self.odom_window[0]],
+                      [self.odom_window[1]], '-b', label='Filtered')[0],
+                      self.ax1.plot([self.movAvg_window[0]],
+                      [self.movAvg_window[1]], '-og', label='Moving Average', linewidth=0.8)[0]]
         self.ani = animation.FuncAnimation(self.fig,
                                            self.animate,
                                            interval=self.interval,
-                                           blit=True)
-
-        plt.ion()
-        plt.legend(labels=["Raw", "Filtered", "Moving Average"])
+                                           blit=False)
+        plt.legend(labels=["Raw", "Filtered", "Moving Average"], handles=self.lines)
         plt.title('Live Comparison of Different Absolute Positioning Algorithms')
         plt.show()
 
     def shift_windows(self):
         self.gps_window_start += 1
         self.gps_window_end += 1
-        self.odom__window_start += 1
+        self.odom_window_start += 1
         self.odom_window_end += 1
         self.movAvg_window_start += 1
         self.movAvg_window_end += 1
         # TODO bounds checking
-        self.gps_window = self.gps_data[self.gps_window_start:self.gps_window_end]
-        self.odom_window = self.odom_data[self.odom__window_start:self.odom_window_end]
-        self.movAvg_window = self.movAvg_data[self.movAvg_window_start:self.movAvg_window_end]
+        self.gps_window = [self.gps_data['lon'][self.gps_window_start:self.gps_window_end],
+                           self.gps_data['lat'][self.gps_window_start:self.gps_window_end]]
+        self.odom_window = [self.odom_data['lon'][self.odom_window_start:self.odom_window_end],
+                            self.odom_data['lat'][self.odom_window_start:self.odom_window_end]]
+        self.movAvg_window = [self.movAvg_data['lon']
+                              [self.movAvg_window_start:self.movAvg_window_end],
+                              self.movAvg_data['lat']
+                              [self.movAvg_window_start:self.movAvg_window_end]]
 
     def animate(self, frame, *args):
-        self.shift_windows()
         self.lines[0].set_data(self.gps_window)
         self.lines[1].set_data(self.odom_window)
         self.lines[2].set_data(self.movAvg_window)
-        return self.lines
+        self.shift_windows()
+        return [x for x in self.lines]
 
 
 def main():
